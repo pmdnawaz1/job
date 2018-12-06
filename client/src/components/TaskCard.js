30649b0fa34924447e2472e71e5583b5bbe861db
Andy <wang.andy32@gmail.com>
2018-12-06 11:52:51 -0500

    added dynamic checkboxes

--- client/src/components/TaskCard.js
+++ client/src/components/TaskCard.js
@@ -2,10 +2,28 @@ import React from 'react'
 
 
 class TaskCard extends React.Component{
+
+  state={
+    iconClass:"task-check far fa-square"
+  }
+
+  toggleSquare = (e) => {
+    if (this.state.iconClass === "task-check far fa-square") {
+      this.setState({
+        iconClass: "task-check far fa-check-square"
+      })
+    }else{
+      this.setState({
+        iconClass:"task-check far fa-square"
+      })
+    }
+  }
+
   render(){
     return(
-      <div className="task" >
-        <p>{this.props.info.content}- {this.props.info.due_date}</p>
+      <div className="task"  >
+        <h3>{this.props.info.content}<i onClick={this.toggleSquare} className={this.state.iconClass}></i></h3>
+        <small>Due Date: {this.props.info.due_date}</small>
       </div>
     )
   }
